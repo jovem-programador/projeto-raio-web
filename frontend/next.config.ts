827a42f69cf2496a4b3ef6fe04b7ae4616f1f9c8
Anderson Marley <andersonmarley011@gmail.com>
2026-04-13 10:26:00 -0300

    fix: correção de rota

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Aumenta o limite do body para requisições que passam pelo proxy
-  allowedDevOrigins: ["192.168.16.115", "localhost:3000"],
+  allowedDevOrigins: ["raio.boingaestrutural.com", "192.168.15.102", "192.168.16.115", "localhost:3000", "127.0.0.1:3000"],
   
   experimental: {
     middlewareClientMaxBodySize: 524288000, // 500MB em bytes
